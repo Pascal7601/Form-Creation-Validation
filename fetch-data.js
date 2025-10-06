2ad1e384acded95fd818b3c48321fd9d84e0ddb3
Pascal7601 <levispascal63@gmail.com>
2025-10-06 11:56:29 +0300

    send a get request to an api to retrieve names of certain characters and display them

---
+++ fetch-data.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    async function fetchUserData() {
+        const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+        const dataContainer = document.getElementById("api-data");
+        try {
+            const response = await fetch(apiUrl);
+            if(!response.ok) {
+                throw new Error("failed to fetch data from url");
+            }
+            const users = await response.json();
+            dataContainer.innerHTML = "";
+            const userList = document.createElement("ul");
+
+            users.forEach((user) => {
+                let li = document.createElement("li");
+                li.textContent = user.name;
+                userList.append(li);
+            });
+            dataContainer.append(userList);
+        } catch(error) {
+            dataContainer.innerHTML = "";
+            dataContainer.textContent = "Failed to load user data.";
+            console.log("failed to fetch data", error);
+        }
+    }
+    fetchUserData();
+})
